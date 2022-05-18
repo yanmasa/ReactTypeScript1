91fa26b80fd402bfc92e6f415d19ef0f2bdfde7b
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-18 09:20:52 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,18 @@
+/*  eslint-disable @typescript-eslint/no-unused-vars */
+
+let boolean: boolean = true;
+let number: number = 3;
+let str: string = "A";
+let array1: Array<number> = [1, 2, 3];
+let array2: number[] = [1, 2, 3];
+let tuple: [number, string] = [0, "A"];
+let any1: any = false;
+const func = (): void => {
+  console.log("戻り値なし");
+};
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "Tom" };
