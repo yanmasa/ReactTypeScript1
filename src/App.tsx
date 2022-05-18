c375d12ae2dab4f3054f0a9c21af6bccd7a8aa95
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-18 10:03:18 +0900

    Practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Practice1 } from "./paractices/Practice1";
+import { Practice2 } from "./paractices/Practice2";
+import { Practice3 } from "./paractices/Practice3";
+import { Practice4 } from "./paractices/Practice4";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
     </div>
   );
 }
---
+++ src/paractices/Practice4.tsx
@@ -0,0 +1,13 @@
+export const Practice4 = () => {
+  const calcTotalFee = (num) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+  const onClickPractice = () => calcTotalFee(1000);
+  return (
+    <>
+      <p>練習問題:設定ファイル</p>
+      <button onClick={onClickPractice}>練習問題4を実行</button>
+    </>
+  );
+};
